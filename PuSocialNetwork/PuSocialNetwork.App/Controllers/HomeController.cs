42c49be1c2a5f1b049bde2a737f8d7242e3d0ada
l3kov9 <l3kov.9@gmail.com>
2019-01-11 17:10:29 +0200

    Login Page implemented with UI

---
+++ PuSocialNetwork/PuSocialNetwork.App/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿namespace PuSocialNetwork.App.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using PuSocialNetwork.App.Models;
+    using System;
+    using System.Diagnostics;
+    using System.Net;
+
+    public class HomeController : Controller
+    {
+        private const string QuotesUrl = @"http://nvp-playground.azurewebsites.net/api/quotes/random/txt";
+
+        public IActionResult Index()
+        {
+            var client = new WebClient();
+            var quoteAsText = client.DownloadString(QuotesUrl);
+
+            var quoteLines = quoteAsText.Split(Environment.NewLine);
+
+            return View(nameof(Index), quoteLines);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PuSocialNetwork/PuSocialNetwork.Data/SocialNetworkDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace PuSocialNetwork.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class SocialNetworkDbContext : DbContext
+    {
+        public SocialNetworkDbContext(DbContextOptions<SocialNetworkDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
