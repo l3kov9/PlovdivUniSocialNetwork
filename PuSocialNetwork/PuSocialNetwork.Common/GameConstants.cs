a57d89f18d52812799215b6dae4d333d6e1b684c
l3kov9 <l3kov.9@gmail.com>
2019-03-21 16:28:14 +0200

    Configuration string moved to hidden input

--- PuSocialNetwork/PuSocialNetwork.Common/GameConstants.cs
+++ PuSocialNetwork/PuSocialNetwork.Common/GameConstants.cs
@@ -5,7 +5,5 @@
         public static int FieldSize = 4;
 
         public static int TotalRndNumbersForStart = 2;
-
-        public static string ConnectinString = "Server=.;Database=Game2048Db;Integrated Security=True;";
     }
 }
